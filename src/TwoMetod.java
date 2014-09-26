f6959fd802198e4cc290aa871141adc91966314e
Anastasiya <morina.anastasiya@gmail.com>
2014-09-26 23:05:53 +0400

    Initial commit

---
+++ src/TwoMetod.java
@@ -0,0 +1,39 @@
+/**
+ * Created by ALEX on 26.09.2014.
+ */
+public class TwoMetod {
+    public static void main(String[] args) {
+        int [] x = {1,2,3,4,5};
+        System.out.print("Первый массив: ");
+        for(int i = 0; i < x.length; i++) {
+            System.out.print(x[i]);
+        }
+        System.out.println();
+        int [] y = {5,6,7,8,9};
+        System.out.print("Второй массив: ");
+        for(int i = 0; i < y.length; i++) {
+            System.out.print(y[i]);
+        }
+        System.out.println();
+        int [] z = three (x,y);
+    }
+    public static int [] three (int [] x, int [] y) {
+        System.out.print("Длина будущего массива: ");
+        int [] z = new int[x.length+y.length];
+        int length = z.length;
+        System.out.println(length);
+        System.out.print("Склеенный массив: ");
+        for (int i=0; i<z.length; i=i+1) {
+            if (i<x.length) {
+                z[i] = x[i];
+            } else {
+                z[i] = y[i-x.length];
+            }
+            System.out.print(z[i]);
+        }
+        return (z);
+    }
+}
+
+
+
